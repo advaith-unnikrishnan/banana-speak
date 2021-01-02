7deab01a8466f2029f05ab114fe7e9b02546790d
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2021-01-02 18:04:38 +0530

    wip: api cal for mock server

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var textInput=document.querySelector("textarea")
-var textOutput=document.querySelector("#txt-output")
+var textOutput=document.querySelector("#output")
+
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslateUrl(text){
+    return serverUrl+"?text="+text
+}
+
+function eventHandler(){
+    var text= textInput.value
+    var url= getTranslateUrl(text)
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+}
 
 
 
 btnTranslate.addEventListener("click",eventHandler)
 
-function eventHandler(){
-    console.log("clicked")
-    textOutput.innerText = textInput.value
-}
